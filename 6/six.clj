17d26a886da6016f40ed227dfa78a9e09c20f6bc
Ho-Yon Mak <hoyon.mak@gmail.com>
2020-12-06 18:18:37 +0000

    using comp

--- 6/six.clj
+++ 6/six.clj
@@ -6,26 +6,24 @@
   (-> (slurp "input")
       (str/split #"\n\n")))
 
-(defn process-form-1 [formdata]
+(defn process-form1 [formdata]
   (let [letters (str/replace formdata #"\s" "")]
     (distinct letters)))
 
 (defn part1 [forms]
-  (->> (read-file)
-       (map process-form-1)
-       (map count)
-       (reduce + 0)))
+  (->> forms
+       (map (comp count process-form1))
+       (reduce +)))
 
-(defn process-form-2 [formdata]
+(defn process-form2 [formdata]
   (let [forms (str/split formdata #"\n")
         sets (map #(into #{} %) forms)]
     (apply set/intersection sets)))
 
 (defn part2 [forms]
   (->> forms
-       (map process-form-2)
-       (map count)
-       (reduce + 0)))
+       (map (comp count process-form2))
+       (reduce +)))
 
 (defn -main []
   (let [forms (read-file)]
